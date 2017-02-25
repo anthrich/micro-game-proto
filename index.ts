6b9b2b4d6e569ea9f1108200ffa03a6e2faf3287
Anth Richardson <anthrblahblah@gmail.com>
2017-02-25 01:04:43 +0000

    init

---
+++ index.ts
@@ -0,0 +1,25 @@
+import * as path from 'path';
+import * as express from 'express';
+import { createServer } from 'http';
+import { Server } from 'colyseus';
+
+// Require ChatRoom handler
+import { GameRoom } from "./rooms/gameRoom";
+
+const port = process.env.PORT || 3553;
+const app = express();
+
+// Create HTTP Server
+const httpServer = createServer(app);
+
+// Attach WebSocket Server on HTTP Server.
+const gameServer = new Server({ server: httpServer });
+
+// Register ChatRoom as "chat"
+gameServer.register("game_room", GameRoom);
+
+app.use(express.static(path.join(__dirname, "static")));
+
+httpServer.listen(port);
+
+console.log(`Listening on http://localhost:${ port }`);
---
+++ rooms/gameRoom.ts
@@ -0,0 +1,89 @@
+import {Room} from "colyseus";
+import GameObject from '../../gremmage/src/js/game-engine/game-object';
+import MovementComponent from '../../gremmage/src/js/game-engine/movement-component';
+
+class ServerGameObject extends GameObject {
+	
+	clientId: string;
+	
+	constructor(clientId: string) {
+		super();
+		this.addComponent(new MovementComponent())
+		this.clientId = clientId;
+	}
+}
+
+export class GameRoom extends Room<any> {
+	
+	gameState: any;
+	delta: number;
+	
+	constructor(options) {
+		super(options)
+		
+		// Broadcast patched state to all connected clients at 20fps (50ms)
+		this.setPatchRate(1000 / 20)
+		// Call game simulation at 60fps (16.6ms)
+		this.delta = 1000 / 60;
+		this.setSimulationInterval(this.tick.bind(this), this.delta)
+		
+		// // Call this function if you intend to implement delay compensation
+		// // techniques in your game
+		// this.useTimeline()
+		this.gameState = {gameObjects: []};
+		this.setState(this.gameState)
+	}
+	
+	requestJoin(options) {
+		// only allow 10 clients per room
+		return this.clients.length < 10;
+	}
+	
+	onJoin(client) {
+		console.log(client.id, "joined game!");
+		let newPlayer = new ServerGameObject(client.id);
+		this.gameState.gameObjects.push(newPlayer);
+	}
+	
+	onLeave(client) {
+		console.log(client.id, "left game!");
+	}
+	
+	onMessage(client, data) {
+		console.log(client.id, "sent message on GameRoom")
+		var currentObject = this.gameState.gameObjects
+			.find((go) => {
+				return go.clientId === client.id
+			});
+		currentObject.components
+			.forEach((c) => {
+				let mc = c as MovementComponent;
+				mc.targetPosition.x = data.x;
+				mc.targetPosition.y = data.y;
+			})
+	}
+	
+	onDispose() {
+		console.log("Dispose ChatRoom");
+	}
+	
+	tick() {
+		this.gameState.gameObjects.forEach((go) => {
+			go.update(this.delta);
+		});
+		// This is your 'game loop'.
+		// Inside function you'll have to run the simulation of your game.
+		//
+		// You should:
+		// - move entities
+		// - check for collisions
+		// - update the state
+		//
+		
+		// // Uncomment this line to see the simulation running and clients receiving the patched state
+		// // In this example, the server simply adds the elapsedTime every 2 messages it receives
+		// if ( this.state.messages.length % 3 == 0 ) {
+		//   this.state.messages.push(`${ this.clock.elapsedTime }: even`)
+		// }
+	}
+}
