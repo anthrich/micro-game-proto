0ae9da367e8a13c9d89fa006bf15e954d542de77
John Richardson <johnrich85@hotmail.com>
2017-03-04 21:36:07 +0000

    Added colors to factory.

--- player/playerFactory.ts
+++ player/playerFactory.ts
@@ -5,9 +5,14 @@ import {Player} from '../../gremmage/src/js/example-app/player';
 export class PlayerFactory {
 
     playerCount : number;
+    colors : Array<string>;
 
     constructor() {
         this.playerCount = 0;
+
+        this.colors = Array<string>();
+        this.colors.push('blue');
+        this.colors.push('yellow');
     }
 
     /**
@@ -18,7 +23,7 @@ export class PlayerFactory {
      */
     make(clientId : string, updateGameState : Function) : Player
     {
-        var player = new Player(this.playerCount, clientId);
+        var player = new Player(this.playerCount, this.colors[this.playerCount], clientId);
 
         this.playerCount ++;
 
