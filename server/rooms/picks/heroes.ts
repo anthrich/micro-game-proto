57da19419142fef5e9d100ea24a3d0ebe3f6c7e3
Anth Richardson <anthrblahblah@gmail.com>
2017-04-22 17:24:55 +0100

    WIP

--- server/rooms/picks/heroes.ts
+++ server/rooms/picks/heroes.ts
@@ -1,18 +1,18 @@
-import HeroPortrait from '../../../game/Hero';
+import {Hero} from '../../../game/Hero';
 
 export default {
     get : () => {
         return [
-            new HeroPortrait(1, "Rifleman", "http://lorempixel.com/200/100/people/7"),
-            new HeroPortrait(2, "Tank Buster", "http://lorempixel.com/200/100/people/9"),
-            new HeroPortrait(3, "Rocketeer", "http://lorempixel.com/200/100/people/3"),
-            new HeroPortrait(4, "Rick Picker", "http://lorempixel.com/200/100/people/8"),
-            new HeroPortrait(5, "Medic", "http://lorempixel.com/200/100/people/2"),
-            new HeroPortrait(6, "Artillery", "http://lorempixel.com/200/100/people/3"),
-            new HeroPortrait(7, "Scout", "http://lorempixel.com/200/100/people/1"),
-            new HeroPortrait(8, "General", "http://lorempixel.com/200/100/people/6"),
-            new HeroPortrait(9, "Tank", "http://lorempixel.com/200/100/people/5"),
-            new HeroPortrait(10, "Humvee", "http://lorempixel.com/200/100/people/4"),
+            new Hero(1, "Rifleman", "http://lorempixel.com/200/100/people/7"),
+            new Hero(2, "Tank Buster", "http://lorempixel.com/200/100/people/9"),
+            new Hero(3, "Rocketeer", "http://lorempixel.com/200/100/people/3"),
+            new Hero(4, "Rick Picker", "http://lorempixel.com/200/100/people/8"),
+            new Hero(5, "Medic", "http://lorempixel.com/200/100/people/2"),
+            new Hero(6, "Artillery", "http://lorempixel.com/200/100/people/3"),
+            new Hero(7, "Scout", "http://lorempixel.com/200/100/people/1"),
+            new Hero(8, "General", "http://lorempixel.com/200/100/people/6"),
+            new Hero(9, "Tank", "http://lorempixel.com/200/100/people/5"),
+            new Hero(10, "Humvee", "http://lorempixel.com/200/100/people/4"),
         ]
     }
 }
